82b5389a3056cade5a8eae4a03d9e41e0cfb1f85
Veronica Herzog <vherzog@bu.edu>
2017-11-16 00:07:20 -0500

    Clean up Arduino test code

--- Arduino/serial_rpi_test/serial_rpi_test.ino
+++ Arduino/serial_rpi_test/serial_rpi_test.ino
@@ -1,11 +1,4 @@
-//char dataString[50] = {0};
 String dataString = "";
-char stringOne[5] = "One!";
-char stringTwo[5] = "Two!";
-int indOne = 0;
-int indTwo = 0;
-int state;
-//int incomingByte = 0;
 void setup() {
   // put your setup code here, to run once:
   state = 0;
@@ -27,36 +20,6 @@ void loop() {
     } else {
       Serial.println("No!");
     }
-    /*if(state == 0) {
-      while(Serial.available() <= 0) {
-        // Initalize state
-        if(dataString == "1") {
-          state = 1;
-        } else if (dataString == "2") {
-          state = 2;
-        }
-        Serial.println("r");
-      }
-    }
-    else if(state) {
-      // Done with message, clear variables
-      if(dataString == "d") {
-        state = 0;
-        dataString = "";
-      }
-      // Sending message
-      else {
-        // Ready for next byte
-        if(state == 1) {
-          Serial.println("One!");
-        } else if(state == 2) {
-          Serial.println("Two!");
-        }
-      }
-    }*/
-  /*} else {
-    Serial.println("Noo!");
-    } */
   delay(500);
   }
 }
